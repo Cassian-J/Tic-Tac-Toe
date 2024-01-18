beae9060b612baba6672d403a774f10d8b132499
cassian.joly@ynov.com <cassian.joly@ynov.com>
2024-01-18 08:37:41 +0100

    test init of connection

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("Voulez vous etre client ou serveur?")
+		fmt.Println("1 serveur")
+		fmt.Println("2 client")
+		option, _ := reader.ReadString('\n')
+		option = strings.TrimSpace(option)
+		switch option {
+		case "1":
+			Server()
+		case "2":
+			Client()
+		}
+	}
+}
+
+func Server() {
+	fmt.Println("serveur lancé")
+	// Listen for incoming connections on port 8080
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Accept incoming connections and handle them
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		// Handle the connection in a new goroutine
+		go HandleConnection(conn)
+	}
+}
+
+func HandleConnection(conn net.Conn) {
+	// Close the connection when we're done
+	defer conn.Close()
+
+	// Read incoming data
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the incoming data
+	fmt.Printf("Received: %s", buf)
+}
+func Client() {
+	fmt.Println("client lancé")
+	// Connect to the server
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Send some data to the server
+	_, err = conn.Write([]byte("Hello, server!"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Close the connection
+	conn.Close()
+}
